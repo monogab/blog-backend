90e7e7ca393b53cb5a3d23f2f9267f9de3a1aefb
Gabor Monostori <monogab@gmail.com>
2018-11-14 15:20:41 +0100

    tutorial test 4, end

--- apps/polls/views.py
+++ apps/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import  get_object_or_404, render
 
 
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Choice, Question
 from django.urls import reverse
 
 def index(request):
@@ -19,8 +19,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
